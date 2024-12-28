9867741df74832edf11f7f4e546a17970902adcb
Dlawe93 <ewald.pruss@outlook.com>
2024-12-28 19:04:04 +0100

    CHANGE

--- contract-manager/run.sh
+++ contract-manager/run.sh
@@ -2,5 +2,5 @@
 
 echo "Vertragsmanager Webserver wird gestartet..."
 
-# Starte den Webserver auf Port 8080, nur für lokale Anfragen
-python3 -m http.server 8080 
+# Starte den Webserver auf Port 8080 und lausche auf alle Netzwerk-Interfaces
+python3 -m http.server 8080 --bind 0.0.0.0
